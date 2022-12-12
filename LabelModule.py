1b6d24102f8cfbad21364175b53fd8135a0eeed6
Changjiang Huang <psxch7@exmail.nottingham.ac.uk>
2022-12-12 04:47:03 +0000

    add font

--- LabelModule.py
+++ LabelModule.py
@@ -2,14 +2,13 @@ import pygame
 
 # copied from Daniel's example
 
-
 class Label:
     def __init__(self, text, pos, size, colour):
         self.text = text
         self.pos = pos
         pygame.font.init()
-        self.font = pygame.font.Font('./fonts/times new roman bold italic.ttf', size)
-        # self.font = pygame.font.SysFont(None, size)
+        # This font is downloaded from the website: https://freefontsfamily.com
+        self.font = pygame.font.Font('./Fonts/times new roman bold italic.ttf', size)
         self.font_colour = pygame.Color(colour)
 
     def draw(self, screen):
